4a2b95fc416241a41a91c9c9d3b1372c1f6453d8
deathgrips <farmakon91@gmail.com>
2017-02-04 16:01:37 +0200

    second

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,41 @@
-$(document).foundation();
+;(function($){
+	"use strict";
+
+	$(document).foundation();
+
+	$(function(){
+		$('.ba-works__slider').slick({
+			dots: true,
+			slide: '.ba-works__slide',
+			prevArrow: '.ba-slider-arrow--prev',
+			nextArrow: '.ba-slider-arrow--next'
+		});
+	})
+
+	$(function(){
+		$('.ba-team__slider').slick({
+			dots: false,
+			slide: '.ba-team__slide',
+			slidesToShow: 3,
+			prevArrow: '.ba-slider-arrow--prev',
+			nextArrow: '.ba-slider-arrow--next',
+			responsive: [
+			    {
+			      breakpoint: 800,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			  ]
+		});
+	})
+
+})(jQuery);
